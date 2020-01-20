ef7f5f449742f185c0d72ee8717f99de3b5e86be
junjoons <martin.junyoung@outlook.com>
2020-01-20 15:57:11 +0900

    Updates

---
+++ precache-manifest.53f1b283bf6ba5886e88397ebe5cccce.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cfbc6c164b7118880455df5f80799c9d",
+    "url": "/youflix/index.html"
+  },
+  {
+    "revision": "709a1b8f97bcd5939fbc",
+    "url": "/youflix/static/css/main.f61c532c.chunk.css"
+  },
+  {
+    "revision": "b8e5cf053a28dc583781",
+    "url": "/youflix/static/js/2.1a440238.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/youflix/static/js/2.1a440238.chunk.js.LICENSE"
+  },
+  {
+    "revision": "709a1b8f97bcd5939fbc",
+    "url": "/youflix/static/js/main.88673a33.chunk.js"
+  },
+  {
+    "revision": "ee8e000cff5cf31b98c2",
+    "url": "/youflix/static/js/runtime-main.d890f1b4.js"
+  },
+  {
+    "revision": "67d8a93edac0f4e68adb36cc78c10fe1",
+    "url": "/youflix/static/media/youflix_logo.67d8a93e.png"
+  }
+]);
