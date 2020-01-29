d7fc26ec9fa00a5044429fc9dd3bb500414bcbf7
junjoons <martin.junyoung@outlook.com>
2020-01-30 01:35:37 +0900

    Building Component #3.0 : img component

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -9,10 +9,6 @@ class Home extends React.Component {
     data: []
   };
 
-  helloWorld = () => {
-    console.log("WTF?");
-  };
-
   getData = async term => {
     const response = await youtube.get("search", {
       params: {
@@ -22,52 +18,72 @@ class Home extends React.Component {
         q: term
       }
     });
-    console.log(response);
-    return response;
-  };
-
-  renderData = () => {
+    // console.log(response.data);
+    console.log(response.data.items[0].snippet.thumbnails.default.url);
+    const picURL = response.data.items[0].snippet.thumbnails.default.url;
+    const { data } = this.state;
+    this.setState({ data: data.concat(picURL) });
     const channelData = require("../data/data.json");
-    channelData.forEach((channel, index) => {
-      //console.log(this.getData(channel.name));
-      if (index === channelData.length - 1) {
-        this.setState({ isLoading: false });
-      }
-    });
+    if (this.state.data.length === channelData.length) {
+      this.setState({ isLoading: false });
+    }
+    // return response;
   };
 
+  // renderData = () => {
+  //   const channelData = require("../data/data.json");
+  //   channelData.forEach((channel, index) => {
+  //     //console.log(this.getData(channel.name));
+  //     if (index === channelData.length - 1) {
+  //       this.setState({ isLoading: false });
+  //     }
+  //   });
+  // };
+  componentDidMount() {
+    const channelData = require("../data/data.json");
+    channelData.map(channel => this.getData(channel.name));
+    console.log("done!");
+  }
   render() {
     const channelData = require("../data/data.json");
-    //let { isLoading, data } = this.state;
-    //로딩이 완료되지 않았을떄 APIdata값을 받으려 하기 때문에 값이 제대로 나오지 않음. 고로 state에 isLoading 속성을 만들어 삼항 연산자로 구분하며 rendering하기
-    this.renderData();
-    return (
-      <div className="container">
-        <div className="channels">
-          {channelData.map(channel => {
-            //this.setState({ data: data.concat(this.getData(channel)) });
-            //console.log(data, channelData.length);
-            return (
-              <Link
-                to={{
-                  pathname: `channel:${channel.id}`,
-                  state: {
-                    name: channel.name,
-                    link: channel.channelLink,
-                    description: channel.description
-                  }
-                }}
-                key={channel.id}
-              >
-                <div className="channelName">
-                  <li className="channelName__name">{channel.name}</li>
-                </div>
-              </Link>
-            );
-          })}
+    const { isLoading } = this.state;
+    // let { isLoading, data } = this.state;
+    // **로딩이 완료되지 않았을떄 APIdata값을 받으려 하기 때문에 값이 제대로 나오지 않음. 고로 state에 isLoading 속성을 만들어 삼항 연산자로 구분하며 rendering하기
+    // this.renderData();
+    if (isLoading === false) {
+      return (
+        <div className="container">
+          <div className="channels">
+            {channelData.map((channel, index) => {
+              //this.setState({ data: data.concat(this.getData(channel)) });
+              //console.log(data, channelData.length);
+              // this.getData(channel.name);
+              // console.log(index)
+              return (
+                <Link
+                  to={{
+                    pathname: `channel:${channel.id}`,
+                    state: {
+                      name: channel.name,
+                      link: channel.channelLink,
+                      description: channel.description
+                    }
+                  }}
+                  key={channel.id}
+                >
+                  <div className="channelName">
+                    <img src={this.state.data[index]} />
+                    <li className="channelName__name">{channel.name}</li>
+                  </div>
+                </Link>
+              );
+            })}
+          </div>
         </div>
-      </div>
-    );
+      );
+    } else {
+      return <span>loading...</span>;
+    }
   }
 }
 
