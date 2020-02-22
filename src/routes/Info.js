0286d8a08d9389d588051936669f25fb10ba8a38
junjoons <martin.junyoung@outlook.com>
2020-02-22 10:35:41 +0900

    Building component #3

--- src/routes/Info.js
+++ src/routes/Info.js
@@ -19,14 +19,27 @@ class Info extends React.Component {
         }
       } = this.props;
       return (
-        <div className="info">
-          <img className="info__img" src={thumbnail} alt={name} />
-          <div className="info__info">
-            <span className="info__description">{description}</span>
-            <h1 className="info__name">{name}</h1>
+        <div className="info_container">
+          <div className="info">
+            {/* <img src={banner} alt={name} /> */}
+            <img className="info__img" src={thumbnail} alt={name} />
+            <div className="info__info">
+              <span className="info__description">{description}</span>
+              <h1 className="info__name">{name}</h1>
+            </div>
+            {/* <a className="info__link" href={link}> */}
+            {/* </a> */}
           </div>
-          {/* <a className="info__link" href={link}> */}
-          {/* </a> */}
+          <div className="video_container">
+            {/* <iframe
+              id="ytplayer"
+              type="text/html"
+              width="640"
+              height="360"
+              src="https://www.youtube.com/embed/M7lc1UVf-VE?autoplay=1&origin=http://example.com"
+              frameborder="0"
+            ></iframe> */}
+          </div>
         </div>
       );
     } else {
