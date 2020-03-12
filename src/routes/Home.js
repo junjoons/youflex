17adbf1d684c01df97824b2bd2e716f30f42d4a6
junjoons <martin.junyoung@outlook.com>
2020-03-12 11:25:29 +0900

    20200312 React

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -10,50 +10,66 @@ class Home extends React.Component {
     raw_snippetData: []
   };
 
-  getSnippetData = async term => {
-    const response = await youtube.get("channels", {
-      params: {
-        part: "snippet",
-        maxResults: 1,
-        key: "AIzaSyCHi-dsz2j6aoMSm2Sb9qr459qnz_9-S2g",
-        id: term
-      }
-    });
-    return response;
-  };
-
-  renderSnippetData = async term => {
-    const response = await this.getSnippetData(term);
-    console.log(response);
-    this.setState({
-      snippetData: this.state.snippetData.concat([
-        [
-          {
-            name: response.data.items[0].snippet.title,
-            id: response.data.items[0].id,
-            description: response.data.items[0].snippet.description,
-            thumbnail: response.data.items[0].snippet.thumbnails.default.url,
-            high_thumbnail: response.data.items[0].snippet.thumbnails.high.url
+  getData = id => {
+    const channelData = require("../data/data.json");
+    channelData.map((channel, index) => {
+      function _promise() {
+        return new Promise(function(resolve, reject) {
+          const response = youtube.get("channels", {
+            params: {
+              part: "snippet",
+              maxResults: 1,
+              key: "AIzaSyBMlsbAuI4UsGFE24TDNwb5aE7byOo2KCw",
+              id: channel.channel__id
+            }
+          });
+          if (response) {
+            resolve(response);
+            console.log("API number " + index + " pre-pre-fetched!");
+          } else {
+            reject(
+              Error("failed on fetching API data. was api quota exeeded?")
+            );
           }
-        ]
-      ]),
-      raw_snippetData: this.state.raw_snippetData.concat([response])
+        });
+      }
+      _promise(true)
+        .then(
+          response => {
+            console.log("API number " + index + " pre-fetched!");
+            this.setState({
+              snippetData: this.state.snippetData.concat([
+                [
+                  {
+                    name: response.data.items[0].snippet.title,
+                    id: response.data.items[0].id,
+                    description: response.data.items[0].snippet.description,
+                    thumbnail:
+                      response.data.items[0].snippet.thumbnails.default.url,
+                    high_thumbnail:
+                      response.data.items[0].snippet.thumbnails.high.url
+                  }
+                ]
+              ]),
+              raw_snippetData: this.state.raw_snippetData.concat([response])
+            });
+            console.log("API number " + index + " fetched!");
+          },
+          function(error) {
+            console.log(error);
+          }
+        )
+        .then(() => {
+          if (channelData.length - 1 === index) {
+            this.setState({ isLoading: false });
+            console.log("Loading Finished!");
+          }
+        });
     });
-    console.log(this.state);
   };
 
   componentDidMount() {
-    const channelData = require("../data/data.json");
-    const arrayLength = channelData.length - 1;
-    channelData.map(async (channel, index) => {
-      await this.renderSnippetData(channel.channel__id);
-      console.log(index, arrayLength);
-      if (index === arrayLength) {
-        this.setState({ isLoading: false });
-        console.log(this.state);
-      }
-      return null;
-    });
+    this.getData();
   }
   render() {
     const { isLoading, snippetData, raw_snippetData } = this.state;
@@ -63,13 +79,12 @@ class Home extends React.Component {
         <div className="container">
           <div className="channels">
             {snippetData.map((data, index) => {
-              console.log(data);
               const id = data[0].id;
               const title = data[0].name;
               const description = data[0].description;
               const thumbnail = data[0].thumbnail;
               const high_thumbnail = data[0].high_thumbnail;
-              // console.log(id, title, description, thumbnail, high_thumbnail);
+
               return (
                 <Link
                   to={{
